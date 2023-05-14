5922c72c06fc8daea72a5db7a0479fb9a9a616fb
RharanaDev <rharana2188@gmail.com>
2023-05-14 19:31:28 +0200

    GitMiner 1.1.0 -Added General Exception control

---
+++ src/main/java/aiss/GitMiner/exception/IssueNotFoundException.java
@@ -0,0 +1,8 @@
+package aiss.GitMiner.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Issue not found")
+public class IssueNotFoundException extends Exception{
+}
